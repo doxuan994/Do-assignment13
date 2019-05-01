4f8d3ae37cd34d5bd49a999f5456ee5ad9231aaa
Xuan Do <doxuancntp@gmail.com>
2019-05-01 17:13:33 -0400

    Commit.

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// Move sidebar on scroll
+let sidebar = document.getElementById('sidebar');
+let navbar = document.getElementById('navbar');
+let footer = document.getElementById('footer');
+
+
+let header = document.getElementById('header');
+let headeroffsetHeight = header.offsetHeight;
+
+
+let windowInnerWidth = window.innerWidth;
+window.addEventListener('scroll', function() {
+    let footerClientY = footer.getBoundingClientRect().top;
+    let footerClientHeight = footer.clientHeight;
+    // Stop adding the affix class to navbar when users reach the end of the content.
+    let meetFooter = footerClientY - footerClientHeight;
+
+    if (windowInnerWidth >= 1200 && pageYOffset > headeroffsetHeight  && meetFooter > 0) {
+        // Add data-spy="affix" to navbar.
+        navbar.classList.add("affix");
+    } else {
+        navbar.classList.remove("affix");
+    }
+}, false);
+
+
+
+// Tooltip jQuery.
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip({
+        placement: 'top',
+        html: true
+    });
+});
